520117381915cf07782fc9cc395e582459a4bbd4
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2023-10-02 13:03:52 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const params = {
+    access_key: '9c360f87e4c049898da6e0e7b7076fad',
+    query: '91601'
+  };
+
+  const apiUrl = 'http://api.weatherbit.io/v2.0/current';
+
+  //construct url with query params
+  const url = `${apiUrl}?postal_code=${params.query}&country=US&key=${params.access_key}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(`Current Temperatture in ${data.location.name} is ${data.current.temperature}℃`)
+      console.log(data);
+    });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
